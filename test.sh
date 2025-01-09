9de62ab758e90ab827a425a730d9148f62000a58
Jeremiah Russell <jerry@jrussell.ie>
2025-01-09 16:50:50 +0000

    chore(test): update test script to check zola version instead of multiple rust and tool versions

--- test.sh
+++ test.sh
@@ -2,23 +2,8 @@
 
 set -e
 
-echo "---------rust versions-------------------"
-rustup show | grep stable
-rustup show | grep beta
-rustup show | grep nightly
-rustup show | grep "${MIN_RUST}"
-rustup show | grep -m 1 "${RUST_VERSION}"
-echo "---------tools versions------------------"
-cargo release --version
-cargo audit --version
-cargo llvm-cov --version
-cargo nextest --version | sed '2,$s/^/  /'
-cargo expand --version
-nextsv --version
-pcu --version
-circleci-junit-fix --version
-wasm-pack --version
-wasmtime --version
+echo "---------zola version-------------------"
+zola --version
 echo "---------binaries------------------------"
 ls -l /usr/local/cargo/bin/*
 echo "---------end-----------------------------"
